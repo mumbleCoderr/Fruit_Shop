8d8236ab6f39bcd787833c323a8d7d5984d16ede
pragma once <mat02biernat@gmail.com>
2025-01-03 22:17:07 +0100

    configured basic spring security & secured endpoints

--- backend/src/main/java/org/example/backend/repositories/ProductRepository.java
+++ backend/src/main/java/org/example/backend/repositories/ProductRepository.java
@@ -4,7 +4,7 @@ import org.example.backend.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
