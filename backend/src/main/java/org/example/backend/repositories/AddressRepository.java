e127d352c779c87e5f9aa786068161f38203b463
pragma once <mat02biernat@gmail.com>
2025-01-03 15:22:39 +0100

    deleted some entities and changed autoincrementation

--- backend/src/main/java/org/example/backend/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.repositories;
-
-import org.example.backend.entities.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-}
