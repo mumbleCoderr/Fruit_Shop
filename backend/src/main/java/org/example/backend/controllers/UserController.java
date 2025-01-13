c0d54976d7130bf588825ea967c8d90967660dfc
pragma once <mat02biernat@gmail.com>
2025-01-13 19:50:25 +0100

    done changing pasword endpoint

--- backend/src/main/java/org/example/backend/controllers/UserController.java
+++ backend/src/main/java/org/example/backend/controllers/UserController.java
@@ -1,14 +1,13 @@
 package org.example.backend.controllers;
 
 import lombok.AllArgsConstructor;
+import org.example.backend.dtos.NewPasswordDto;
 import org.example.backend.entities.User;
 import org.example.backend.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,21 +17,8 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-
-
-    @GetMapping("/me")
-    public ResponseEntity<User> authenticatedUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        User currentUser = (User) authentication.getPrincipal();
-
-        return ResponseEntity.ok(currentUser);
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<List<User>> allUsers() {
-        List <User> users = userService.allUsers();
-
-        return ResponseEntity.ok(users);
+    @PostMapping("/changepassword")
+    public ResponseEntity<Void> changePassword(@RequestBody NewPasswordDto newPasswordDto) throws Exception {
+        return userService.changePassword(newPasswordDto);
     }
 }
